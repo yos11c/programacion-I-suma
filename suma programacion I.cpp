9f5e3a36e4443e12709b49b76f32bec78e9eac1c
yos11c <ycontrerasc3@miumg.edu.gt>
2022-02-19 11:12:06 +0000

    codigo de suma programacion I

---
+++ suma programacion I.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int suma(int, int );
+void procedimiento1();
+int variableGlobal;
+const string SECCION="A";
+
+int main() {
+    procedimiento1();
+    cout<<"Ingrese el primer numero";
+    int primerNumero, segundoNumero;
+    cin>>primerNumero;
+    cout<<"Ingrese el segundo numero";
+    cin>>segundoNumero;
+
+    suma(primerNumero,segundoNumero);
+    //int resultado=suma(4,5);
+    //cout<<"el resultado es : "<< resultado<<endl;
+    cout<<variableGlobal;
+    procedimiento1();
+    return 0;
+}
+
+int suma(int primerNumero, int segundoNumero){
+    //int resultado= primerNumero+segundoNumero; variable local
+    //return primerNumero+segundoNumero;
+    variableGlobal= primerNumero+segundoNumero;//variable global
+    return variableGlobal;
+}
+
+void procedimiento1(){
+    cout<<" Hola, se ejecuta el procedimiento  "<< "Progra con C++ primer ciclo seccion A. Nombre: Yulmi Contreras" <<SECCION<< endl;
+}
